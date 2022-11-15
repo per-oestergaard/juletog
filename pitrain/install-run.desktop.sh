94a863f168a59e4c18ab2c9f88bfb9918272f66b
Per Østergaard <per+github@post42.dk>
2022-11-15 18:21:07 +0100

    works

--- pitrain/install-run.desktop.sh
+++ pitrain/install-run.desktop.sh
@@ -1,5 +1,7 @@
-mkdir $HOME/.config/autostart
+mkdir -p $HOME/.config/autostart
 
 SCRIPT_PATH=`readlink -f "$0"`
 SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
 cp $SCRIPT_DIR/run.desktop $HOME/.config/autostart/run.desktop
+cp $SCRIPT_DIR/lxterminal.desktop $HOME/.config/autostart/lxterminal.desktop
+chmod +x $SCRIPT_DIR/run.sh
--- pitrain/setup.sh
+++ pitrain/setup.sh
@@ -1,7 +1,9 @@
 pip install buildhat
-# pip install keyboard
+pip install keyboard
+sudo pip install buildhat
+sudo pip install keyboard
 # pip install tkinter
-pip install pynput
+# pip install pynput
 SCRIPT_PATH=`readlink -f "$0"`
 SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
-bash $SCRIPT_DIR/install-run.desktop.sh
+# bash $SCRIPT_DIR/install-run.desktop.sh
