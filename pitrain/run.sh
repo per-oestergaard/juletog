ec233c429ce7603ec09e3728b29df74d96260ffe
Per Østergaard <per+github@post42.dk>
2022-12-25 09:47:40 +0100

    logging

--- pitrain/run.sh
+++ pitrain/run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-echo "run.sh"
+echo "`date +%H:%M:%S` run.sh"
 
 SCRIPT_PATH=`readlink -f "$0"`
 SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
 LOG_DIR="$SCRIPT_DIR/logs"
 mkdir -p $LOG_DIR
-log=date +run-%m%d.log
-err=date +run-%m%d.err
+log="$LOG_DIR/`date +run-%m%d.log`"
+err="$LOG_DIR/`date +run-%m%d.err`"
+echo "`date +%H:%M:%S` Log is $log"
+echo "`date +%H:%M:%S` Err is $err"
 
 
 while :
 do
-echo "run2.sh"
-$SCRIPT_DIR/run2.sh >> %log% 2>> %err%
+echo "`date +%H:%M:%S` run2.sh"
+bash $SCRIPT_DIR/run2.sh >> $log 2>> $err
+echo "`date +%H:%M:%S` sleep 10"
 sleep 10s
 done
--- pitrain/run2.sh
+++ pitrain/run2.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-
-echo "run2.sh"
+set -x
+echo "`date +%H:%M:%S` run2.sh"
 
 SCRIPT_PATH=`readlink -f "$0"`
 SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
-LOG_DIR="$SCRIPT_DIR/logs"
-mkdir -p $LOG_DIR
 
 while :
 do
-echo "git pull"
+date %m%d
+echo "`date +%H:%M:%S` git pull"
 git pull
-echo "python testhat.py"
-python pitrain/testhat.py || { sudo mpg321 -g 50 ./pitrain/sounds/No-HAT.mp3; sleep 10; sudo reboot now; }
-echo "python run.py"
+echo "`date +%H:%M:%S` python testhat.py"
+python $SCRIPT_DIR/testhat.py || { sudo mpg321 -g 50 $SCRIPT_DIR/sounds/No-HAT.mp3; sleep 120; sudo reboot now; }
+echo "`date +%H:%M:%S` python run.py"
 sudo python $SCRIPT_DIR/run.py
-echo "sleep"
+echo "`date +%H:%M:%S` sleep"
 sleep 10s
 done
